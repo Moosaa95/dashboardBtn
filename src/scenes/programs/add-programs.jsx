33f0a4c9f8f4ec142cdd869957cb38f13e21558f
moosaa95 <moosaabdullahi45@gmail.com>
2022-10-23 17:24:23 +0200

    new file

--- src/scenes/programs/add-programs.jsx
+++ src/scenes/programs/add-programs.jsx
@@ -4,12 +4,11 @@ import { tokens } from "../../theme";
 import { Formik } from "formik";
 import * as yup from "yup";
 import useMediaQuery from "@mui/material/useMediaQuery";
-import { useContext } from "react";
+// import { useContext } from "react";
 import AuthContext from "../context/AuthContext";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import Header from "../../components/Header";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
@@ -20,17 +19,22 @@ const AddProgram = () => {
     const isNonMobile = useMediaQuery("(min-width:600px)");
     const [open, setOpen] = useState(false);
     const [msg, setMsg] = useState("")
+    const [programDescription, setProgramDescription] = useState("")
+    const [programName, setProgramName] = useState("")
+    const [organizerSponsor, setOrganizerSponsor] = useState("")
 
     const theme = useTheme();
     const colors = tokens(theme.palette.mode);
 
     const {addProgram, success} = useContext(AuthContext)
 
-    const [value, setValue] = React.useState(dayjs('2014-08-18T21:11:54'));
+    const [value, setValue] = useState(dayjs('2014-08-18T21:11:54'));
 
-    const handleChange = (newValue) => {
-        setValue(newValue);
-    };
+    // const handleChange = (newValue) => {
+    //     setValue(newValue);
+    // };
+
+    console.log('longest');
 
 
     useEffect(() => {
@@ -54,17 +58,18 @@ const AddProgram = () => {
     setOpen(false);
   };
 
-    const handleFormSubmit = (values) => {
-        // values.preventDefault()
-        [values].map(value => {
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(programName, value);
+        // [values].map(value => {
         addProgram({
-            program_name : value.programName,
-            organizer_sponsor : value.organizerSponsor,
-            program_description : value.programDescription,
-            date_approved : value.dateApproved,
+            program_name : programName,
+            organizer_sponsor : organizerSponsor,
+            program_description : programDescription,
+            date_approved : value.format('DD/MM/YYYY'),
         })
-        })
-        console.log(values);
+        // })
+        // console.log(values);
     };
   return (
     <Box m="20px">
@@ -85,8 +90,8 @@ const AddProgram = () => {
       <Box 
         sx={{ width: "600px", margin: "auto", marginTop: "70px" }}
         >
-        <Formik
-            onSubmit={handleFormSubmit}
+        {/* <Formik
+            onSubmit={e=>handleFormSubmit(e)}
             initialValues={initialValues}
             validationSchema={checkoutSchema}
             sx={{padding: "50px",}}
@@ -98,8 +103,8 @@ const AddProgram = () => {
             handleBlur,
             handleChange,
             handleSubmit,
-            }) => (
-            <form onSubmit={handleSubmit}>
+            }) => ( */}
+            <form onSubmit={e=>handleSubmit(e)}>
                 <Box
                 display="grid"
                 gap="30px"
@@ -113,12 +118,13 @@ const AddProgram = () => {
                     variant="filled"
                     type="text"
                     label="Program Name"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    value={values.programName}
+                    // onBlur={handleBlur}
+                    onChange={e=>setProgramName(e.target.value)}
+                    value={programName}
+                    // value={values.programName}
                     name="programName"
-                    error={!!touched.programName && !!errors.programName}
-                    helperText={touched.programName && errors.programName}
+                    // error={!!touched.programName && !!errors.programName}
+                    // helperText={touched.programName && errors.programName}
                     sx={{ gridColumn: "span 4" }}
                 />
                 <TextField
@@ -126,12 +132,12 @@ const AddProgram = () => {
                     variant="filled"
                     type="text"
                     label="Organizer Sponsor"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    value={values.organizerSponsor}
+                    // onBlur={handleBlur}
+                    onChange={e=>setOrganizerSponsor(e.target.value)}
+                    value={organizerSponsor}
                     name="organizerSponsor"
-                    error={!!touched.organizerSponsor && !!errors.organizerSponsor}
-                    helperText={touched.organizerSponsor && errors.organizerSponsor}
+                    // error={!!touched.organizerSponsor && !!errors.organizerSponsor}
+                    // helperText={touched.organizerSponsor && errors.organizerSponsor}
                     sx={{ gridColumn: "span 4" }}
                 />
                 <TextField
@@ -142,12 +148,12 @@ const AddProgram = () => {
                     variant="filled"
                     type="text"
                     label="Description"
-                    onBlur={handleBlur}
-                    onChange={handleChange}
-                    value={values.programDescription}
+                    // onBlur={handleBlur}
+                    onChange={e=>setProgramDescription(e.target.value)}
+                    value={programDescription}
                     name="programDescription"
-                    error={!!touched.programDescription && !!errors.programDescription}
-                    helperText={touched.programDescription && errors.programDescription}
+                    // error={!!touched.programDescription && !!errors.programDescription}
+                    // helperText={touched.programDescription && errors.programDescription}
                     sx={{ gridColumn: "span 4" }}
                 />
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
@@ -161,8 +167,8 @@ const AddProgram = () => {
                             onChange={(newValue) => {
                                 setValue(newValue);
                             }}
-                            error={!!touched.programDescription && !!errors.programDescription}
-                            helperText={touched.programDescription && errors.programDescription}
+                            // error={!!touched.programDescription && !!errors.programDescription}
+                            // helperText={touched.programDescription && errors.programDescription}
                             sx={{ gridColumn: "span 4" }}
                             renderInput={(params) => <TextField {...params} />}
                         />
@@ -175,8 +181,8 @@ const AddProgram = () => {
                 </Button>
                 </Box>
             </form>
-            )}
-        </Formik>
+            {/* )}
+        </Formik> */}
       </Box>
     </Box>
     )
@@ -187,14 +193,14 @@ export default AddProgram
 const checkoutSchema = yup.object().shape({
     organizerSponsor: yup.string().required("Organizer Sponsor is required"),
     programDescription: yup.string().required("Description is required"),
-    dateApproved: yup.string().required("Invalid Date required"),
+    // dateApproved: yup.string().required("Invalid Date required"),
     programName: yup.string().required("Program name is required"),
     
   });
   const initialValues = {
     organizerSponsor: "",
     programDescription: "",
-    dateApproved: "",
+    // dateApproved: "",
     programName: "",
   };
   
